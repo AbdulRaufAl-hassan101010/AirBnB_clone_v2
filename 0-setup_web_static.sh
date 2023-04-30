2162d6f2b8f41325eb028bd57888a679c596217c
Abdul-Rauf Alhassan <94212640+AbdulRaufAl-hassan101010@users.noreply.github.com>
2023-04-30 11:52:13 +0000

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
-# sets up the web servers for the deployment of web_static
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+# Install Nginx if not already installed
+if [ ! -x "$(command -v nginx)" ]; then
+    apt-get update
+    apt-get -y install nginx
+fi
+
+# Create directories if they don't exist
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Hello World!" > /data/web_static/releases/test/index.html
+
+# Create symbolic link
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user and group
+chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+sed -i '/^\tserver_name _;/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+# Restart Nginx
+service nginx restart
+
+exit 0
