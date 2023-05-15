c53f1cf2b44e2a51d623f901f39588c5f8746128
Abdul-Rauf Alhassan <94212640+AbdulRaufAl-hassan101010@users.noreply.github.com>
2023-05-15 22:54:03 +0000

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+# Import Flask module
+from flask import Flask
 
-from your_flask_app import create_app
+# Create an instance of the Flask class
+app = Flask(__name__)
 
-app = create_app()
-
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+# Define a route and its corresponding view function
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
     return 'Hello HBNB!'
 
+# Run the Flask application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
